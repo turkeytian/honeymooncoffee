32fa8eeecd77cc2bc1097b0424c8d70368af194e
Ruoqi Tian <ruoqitian@Ruoqis-MacBook-Pro.local>
2017-02-17 13:48:36 -0600

    Atom beautify fix

--- home.js
+++ home.js
@@ -1,45 +1,44 @@
 function menu() {
-	$("#menu").slideToggle();
+    $("#menu").slideToggle();
 }
 
-$(document).ready(function() {var iev=0;
+$(document).ready(function() {
+    var iev = 0;
     var ieold = (/MSIE (\d+\.\d+);/.test(navigator.userAgent));
     var trident = !!navigator.userAgent.match(/Trident\/7.0/);
-    var rv=navigator.userAgent.indexOf("rv:11.0");
+    var rv = navigator.userAgent.indexOf("rv:11.0");
 
-    if (ieold) iev=new Number(RegExp.$1);
-    if (navigator.appVersion.indexOf("MSIE 10") != -1) iev=10;
-    if (trident&&rv!=-1) iev=11;
+    if (ieold) iev = new Number(RegExp.$1);
+    if (navigator.appVersion.indexOf("MSIE 10") != -1) iev = 10;
+    if (trident && rv != -1) iev = 11;
 
     // Firefox or IE 11
-    if(typeof InstallTrigger !== 'undefined' || iev == 11) {
+    if (typeof InstallTrigger !== 'undefined' || iev == 11) {
         var lastScrollTop = 0;
         $(window).on('scroll', function() {
             st = $(this).scrollTop();
-            if(st < lastScrollTop) {
+            if (st < lastScrollTop) {
                 console.log('Up');
-            }
-            else if(st > lastScrollTop) {
+            } else if (st > lastScrollTop) {
                 console.log('Down');
-								$('video#intro_video').slideUp();
+                $('video#intro_video').slideUp();
             }
             lastScrollTop = st;
         });
     }
     // Other browsers
     else {
-        $('body').on('mousewheel', function(e){
-            if(e.originalEvent.wheelDelta > 0) {
+        $('body').on('mousewheel', function(e) {
+            if (e.originalEvent.wheelDelta > 0) {
                 console.log('Up');
-            }
-            else if(e.originalEvent.wheelDelta < 0) {
+            } else if (e.originalEvent.wheelDelta < 0) {
                 console.log('Down');
-								$('video#intro_video').slideUp();
+                $('video#intro_video').slideUp();
             }
         });
     }
 
-	$('.hexagon').on('click', function() {
-		$('video#intro_video').slideDown();
-	});
+    $('.hexagon').on('click', function() {
+        $('video#intro_video').slideDown();
+    });
 });
